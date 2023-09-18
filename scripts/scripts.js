4d0a9125d4ebfa44146bc0789bc377f5ccb27ed0
Valentina Valverde <vvgreenville@gmail.com>
2023-09-18 00:15:07 -0400

    Still couldn't make a functional calculator.

---
+++ scripts/scripts.js
@@ -0,0 +1,25 @@
+console.log("test")
+
+
+// const equalsButt = document.querySelector('.equal')
+
+// equalsButt.addEventListener('click', function(num){
+//     console.log("heyoooo")
+    
+//     if(operation === '+'){
+//         result = firstNum + secondNum;
+//         display.value = result;
+//     }
+//     if(operation === '-'){
+//         result = firstNum - secondNum;
+//         display.value = result;
+//     }
+//     if(operation === '*'){
+//         result = firstNum * secondNum;
+//         display.value = result;
+//     }
+//     if(operation === '/'){
+//         result = firstNum / secondNum;
+//         display.value = result;
+//     }
+// });
